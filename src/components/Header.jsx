9bf759ce238e89239d3feb6d3116de07b53815d1
thenikhilsingh <149455116+thenikhilsingh@users.noreply.github.com>
2025-05-29 14:06:58 +0530

    created header

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { NavLink } from "react-router-dom";
+import moon from "../assets/moon.svg";
+
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center p-10 bg-[#8EFDFD] shadow-lg ">
+      <NavLink to="/">
+        <div className="text-2xl font-bold">Nikhil Singh</div>
+      </NavLink>
+
+      <div className="flex items-center gap-10 text-xl ">
+        <NavLink to="/">
+          <div className="hover:text-[#FFB000]">Home</div>
+        </NavLink>
+        <NavLink to="/skills">
+          <div className="hover:text-[#FFB000]">Skills</div>
+        </NavLink>
+        <NavLink to="/works">
+          <div className="hover:text-[#FFB000]">Works</div>
+        </NavLink>
+        <NavLink to="/resume">
+          <div className="hover:text-[#FFB000]">Resume</div>
+        </NavLink>
+        <NavLink to="/contact">
+          <div className="hover:text-[#FFB000]">Contact</div>
+        </NavLink>
+        <div>
+          <img className="size-8" src={moon} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./components/Home.jsx";
+import Skills from "./components/Skills.jsx";
+import Works from "./components/Works.jsx";
+import Resume from "./components/Resume.jsx";
+import Contact from "./components/Contact.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Home />}></Route>
+      <Route path="skills" element={<Skills />}></Route>
+      <Route path="works" element={<Works />}></Route>
+      <Route path="resume" element={<Resume />}></Route>
+      <Route path="contact" element={<Contact />}></Route>
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
