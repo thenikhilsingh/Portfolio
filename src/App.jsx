50e24ba6a6618b7e60e38d48e85feebefbad22c4
thenikhilsingh <149455116+thenikhilsingh@users.noreply.github.com>
2025-08-29 14:31:10 +0530

    UI Change Process starts

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
-import Header from "./components/Header";
-import { Outlet } from "react-router-dom";
-import Footer from "./components/Footer";
-import ScrollToTop from "./components/ScrollToTop";
-import { createContext, useState } from "react";
-
-export const darkModeContext = createContext();
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <darkModeContext.Provider value={{ darkMode, setDarkMode }}>
-        <ScrollToTop />
-        <Header />
-        <Outlet />
-        <Footer />
-      </darkModeContext.Provider>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-import Home from "./components/Home.jsx";
-import Skills from "./components/Skills.jsx";
-import Works from "./components/Works.jsx";
-import Resume from "./components/Resume.jsx";
-import Contact from "./components/Contact.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route path="" element={<Home />}></Route>
-      <Route path="skills" element={<Skills />}></Route>
-      <Route path="works" element={<Works />}></Route>
-      <Route path="resume" element={<Resume />}></Route>
-      <Route path="contact" element={<Contact />}></Route>
-    </Route>
-  )
-);
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
