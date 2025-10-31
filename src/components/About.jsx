6bd9164c2920f61371c846fe415cb2bbec396ba3
thenikhilsingh <nikhilsingh26042004@gmail.com>
2025-10-31 18:12:24 +0530

    about page is responsive now

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -68,18 +68,18 @@ export default function About() {
 
   return (
     <div className="size-full overflow-auto scrollbar-hide ">
-      <div className="flex">
-        <div className="w-[30%] h-[1140px]  bg-[#AAAAAA] bg-[url(/img-about.png)] bg-[center_60%] bg-contain bg-no-repeat flex flex-col justify-between items-center p-11">
-          <div className="bg-black text-white text-4xl font-bold p-3">
+      <div className="flex flex-col lg:flex-row">
+        <div className="lg:w-[30%] w-[100vw] md:w-[100%] h-[95vh] lg:h-[1140px]  bg-[#AAAAAA] bg-[url(/img-about.png)] bg-[center_90px] lg:bg-[center_60%] bg-contain bg-no-repeat flex flex-col justify-between items-center lg:p-11">
+          <div className="bg-black w-full text-center lg:text-left text-white text-4xl font-bold p-5 lg:p-3">
             NIKHIL SINGH
           </div>
           <div className="flex flex-col gap-10 pb-10">
-            <h1 className="text-white font-bold text-5xl text-center">Ready to discuss your project?</h1>
+            <h1 className="text-white font-bold text-3xl lg:text-5xl text-center">Ready to discuss your project?</h1>
             <div> 
               <p className="text-white font-bold text-[15px] text-center">Let's super-charge your business today!</p>
               <NavLink to="/contact">
-                <div className="relative text-black mt-4">
-                  <div className="h-13 w-70 bg-[linear-gradient(90deg,rgba(231,255,135,1)_5%,rgba(207,254,25,1)_40%,rgba(107,222,53,1)_90%)] absolute top-0 left-0"></div>
+                <div className="relative flex  justify-center lg:justify-normal text-black mt-4">
+                  <div className="h-13 w-70 bg-[linear-gradient(90deg,rgba(231,255,135,1)_5%,rgba(207,254,25,1)_40%,rgba(107,222,53,1)_90%)] absolute top-0 lg:left-0"></div>
                   <div className="h-13 w-70 flex items-center justify-center bg-white relative  -translate-x-2 -translate-y-2 hover:translate-x-0 hover:translate-y-0 transition-all duration-300 ease-in-out">
                     <a href="">Get a Free Price Quote</a>
                   </div>
@@ -88,16 +88,16 @@ export default function About() {
             </div>
           </div>
         </div>
-        <div className="w-[70%] h-[1140px]  bg-[#222225] ">
+        <div className="w-[100vw] md:w-[100%] lg:w-[70%] lg:h-[1140px] bg-[#222225]">
           <div className="p-10">
             <h1 className="text-white font-bold text-4xl">
               I'm Specialized in
             </h1>
             <div className="flex items-center">
-              <Icon path={mdiChevronRight} size={2} color="#9898a8" />
+              <Icon className="hidden lg:block" path={mdiChevronRight} size={2} color="#9898a8" />
               <div className=" text-[#A4F025]">
                 <div className="text-center">
-                  <h1 className="text-3xl  font-semibold ">
+                  <h1 className="text-2xl md:text-3xl font-semibold ">
                     {text}
                     <span className="blink text-gray-300">_</span>
                   </h1>
@@ -107,18 +107,18 @@ export default function About() {
           </div>
 
           <div className="flex flex-col gap-[2px] bg-[#3F3F46] shadow-xl/30">
-            <div className="bg-[#1E1E21] p-10 flex flex-col justify-around h-45">
+            <div className="bg-[#1E1E21] p-10 flex flex-col justify-around lg:h-45 gap-6">
               <div className="text-xl text-white border-l-2 border-l-[#A4F025] pl-4 flex items-center">
                 Back-End Development
               </div>
-              <div className="width-full  grid grid-cols-3">
+              <div className="width-full grid lg:grid-cols-3">
                 {backendTechnologies.map((tech, i) => (
                   <div className="flex">
                     <div
-                      data-aos="fade-right"
+                      // data-aos="fade-right"
                       data-aos-delay={i * 200}
                       key={i}
-                      className="flex justify-between items-center w-55"
+                      className="flex justify-between items-center w-full lg:w-55"
                     >
                       <div className="flex gap-2">
                         <img className="size-7" src={tech.logo} alt="" />
@@ -139,18 +139,18 @@ export default function About() {
               </div>
             </div>
 
-            <div className="bg-[#1E1E21] p-10 flex flex-col justify-around h-50 gap-6">
+            <div className="bg-[#1E1E21] p-10 flex flex-col justify-around lg:h-50 gap-6">
               <div className="text-xl text-white border-l-2 border-l-[#A4F025] pl-4 flex items-center">
                 Front-End Development
               </div>
-              <div className="width-full grid grid-cols-3">
+              <div className="width-full grid lg:grid-cols-3">
                 {frontendTechnologies.map((tech, i) => (
                   <div className="flex">
                     <div
-                      data-aos="fade-right"
+                      // data-aos="fade-right"
                       data-aos-delay={i * 200}
                       key={i}
-                      className="flex justify-between items-center w-55"
+                      className="flex justify-between items-center w-full lg:w-55"
                     >
                       <div className="flex gap-2">
                         <img className="size-7" src={tech.logo} alt="" />
@@ -172,9 +172,11 @@ export default function About() {
             </div>
           </div>
 
-          <div data-aos="fade-up" className="p-10">
+          <div 
+          // data-aos="fade-up"
+           className="p-10 flex flex-col gap-5 lg:gap-0">
             {details.map((detail) => (
-              <div className="flex flex-col justify-around h-42">
+              <div className="flex flex-col justify-around lg:h-42">
                 <h1 className="flex text-2xl font-bold gap-2">
                   <span className="text-[#9CA3AF]">{detail.serialNo}</span>
                   <span className="text-white">{detail.heading}</span>
@@ -186,9 +188,9 @@ export default function About() {
         </div>
       </div>
 
-      <div className="bg-white w-full flex justify-around p-10">
+      <div className="bg-white w-full h-[95vh] lg:h-[55vh] flex flex-col lg:flex-row justify-around p-10">
         {careerDetails.map((section, idx) => (
-          <div key={idx} className="w-[40%] flex flex-col">
+          <div key={idx} className="lg:w-[40%] flex flex-col">
             <div className="flex flex-col  gap-10 justify-between h-full">
               <div className="flex flex-col gap-5">
                 <h1 className="text-4xl font-bold">{section.heading}</h1>
