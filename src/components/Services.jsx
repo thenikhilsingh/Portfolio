6aeeaacc6f27aab69fbf8ec968b2f922911687a1
thenikhilsingh <nikhilsingh26042004@gmail.com>
2025-11-03 11:12:25 +0530

    services page is now responive

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,16 +7,17 @@ import { NavLink } from "react-router-dom";
 export default function Services() {
   return (
     <div className="size-full overflow-auto scrollbar-hide ">
-      <div className="flex bg-[#222225]">
+      <div className="flex flex-col lg:flex-row bg-[#222225]">
         <div
-          data-aos="fade-right"
-          className="w-[33%] h-[1140px] text-white p-10 flex flex-col gap-10"
+          // data-aos="fade-right"
+          className=" lg:w-[33%] lg:h-[1140px] text-white lg:p-10 flex flex-col lg:gap-10"
         >
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 p-5 lg:p-0 bg-[#1f1e1e] lg:bg-[#222225]">
             <h1 className="text-4xl font-bold">Services</h1>
             <h1 className="txt-gradient text-4xl font-bold">My Expertise</h1>
           </div>
-          <div className="flex flex-col gap-8">
+          <div className="lg:hidden h-[1px] w-[100vw] bg-gradient-to-r from-blue-500 via-green-400 to-yellow-400"></div>
+          <div className="flex flex-col gap-8 p-5 lg:p-0">
             <div className="flex flex-col gap-5">
               <h1 className="text-xl font-bold">Solving problems</h1>
               <p className="text-xl text-[#d4d0d0]">
@@ -43,7 +44,7 @@ export default function Services() {
             </div>
           </div>
           <NavLink to="/contact">
-            <div className="relative text-black mt-4">
+            <div className="relative text-black mt-4 m-5 lg:m-0">
               <div className="h-13 w-70 bg-[linear-gradient(90deg,rgba(231,255,135,1)_5%,rgba(207,254,25,1)_40%,rgba(107,222,53,1)_90%)] absolute top-0 left-0"></div>
               <div className="h-13 w-70 flex items-center justify-center bg-white relative  -translate-x-2 -translate-y-2 hover:translate-x-0 hover:translate-y-0 transition-all duration-300 ease-in-out">
                 <a href="">Get Free Price Quote</a>
@@ -51,9 +52,11 @@ export default function Services() {
             </div>
           </NavLink>
         </div>
-        <div className="w-[67%] h-[1140px] flex flex-wrap gap-10 p-10">
+        <div className="lg:w-[67%] mb-20 xl:h-[1140px] flex flex-wrap lg:gap-8 lg:p-10 xl:gap-10 p-5 xl:p-10">
           {servicesDetails.map((service) => (
-            <div data-aos="zoom-in" className="w-[47%]">
+            <div 
+            // data-aos="zoom-in" 
+            className="lg:w-[47%]">
               <div className="bg-[linear-gradient(180deg,#b7ff3c,#26d53a)] p-8">
                 <img className="h-12 my-5" src={service.src} alt="" />
                 <h1 className="text-3xl font-bold">{service.heading}</h1>
